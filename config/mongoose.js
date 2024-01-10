d232a430f54ca8497c59502c73ee111a2378222e
Rafeek Biradar <90768585+biradarrafeek@users.noreply.github.com>
2024-01-10 23:22:03 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-
 //connect to DB
 mongoose.connect('mongodb://localhost/hospital_API', { useNewUrlParser: true ,useUnifiedTopology: true });
 const db = mongoose.connection;
