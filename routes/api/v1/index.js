07030d6ccad4e31a0c957067a4f7c0453a5b224e
Rafeek Biradar <90768585+biradarrafeek@users.noreply.github.com>
2024-01-10 23:27:02 +0530

    Update index.js

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -2,8 +2,6 @@ const express= require('express');
 
 const router = express.Router();
 
-
-
 router.use('/doctors', require('./doctors'));
 router.use('/patients', require('./patients'));
 router.use('/reports', require('./reports'));
